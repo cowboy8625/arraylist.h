4cbe20618ad55ec378e3ee954aeb2f1d01cd62c6
cowboy8625 <cowboy8625@yahoo.com>
2024-10-29 08:02:11 -0500

    void* into macro

--- src/main.c
+++ src/main.c
@@ -1,21 +1,23 @@
 #define VECTOR_IMPLEMENTATION
-#include <stdio.h>
 #include "vector.h"
+#include <stdio.h>
+
+DEFINE_VECTOR(int)
 
 int main() {
-    Vector vector;
-    vector_init(&vector, sizeof(int), 4);
+  intVector vector;
+  int_vector_init(&vector, sizeof(int));
 
-    for (int i = 0; i < 10; i++) {
-        vector_push_back(&vector, &i);
-    }
+  for (int i = 0; i < 10; i++) {
+    int_vector_push_back(&vector, i);
+  }
 
-    for (size_t i = 0; i < vector.size; i++) {
-        int *value = (int *)vector_get(&vector, i);
-        printf("%d ", *value);
-    }
-    printf("\n");
+  for (size_t i = 0; i < vector.size; i++) {
+    int value = int_vector_get(&vector, i);
+    printf("%d ", value);
+  }
+  printf("\n");
 
-    vector_free(&vector);
-    return 0;
+  int_vector_free(&vector);
+  return 0;
 }
